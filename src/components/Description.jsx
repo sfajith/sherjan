ae3bee1ba6b3e5af1f82ffa3b9175c76dae5bc00
Sherjan <sfajith@gmail.com>
2024-08-19 15:36:22 -0500

    test width

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -4,7 +4,7 @@ import UpButton from "./UpButton";
 function Description() {
   return (
     <section id="home" className="font-inter-tight p-5 xl:px-[72px]">
-      <div className="2xl:w-[71.5%] m-auto">
+      <div className="2xl:w-[71.5%] m-auto w-full">
       <h3 className="text-[22px] md:text-[28px] font-[500] tracking-[0.02em] mb-3 md:pl-1">
         Hello! Glad you're here!
       </h3>
