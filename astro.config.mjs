a03b28926840168be262a4e26f912c532651832f
Sherjan <sfajith@gmail.com>
2024-08-21 07:26:18 -0500

    deploy6

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,5 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [react(), tailwind()],
   site: 'https://sfajith.github.io',
-  base: '/sherjan/',
+  base: 'sherjan',
 });
